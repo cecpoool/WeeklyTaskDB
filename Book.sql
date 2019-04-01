b6659e4f18bde02ab48b9093e8678078faf9f665
CECILIA COOK <101938034@student.swin.edu.au>
2019-04-01 12:33:34 +1100

    DML added

---
+++ Book.sql
@@ -0,0 +1,10 @@
+﻿ CREATE TABLE [dbo].[Book] (
+    [ISBN] NVARCHAR(30),
+    [Title] NVARCHAR(100),
+    [Year_Published] INT,
+    [Author_ID] INT,
+    [Author_First_Name] NVARCHAR(30),
+    [Author_Last_Name] NVARCHAR(30),
+    [Author_TFN] INT,
+	PRIMARY KEY (ISBN),
+);
---
+++ Student.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [STUDENT_ID] NVARCHAR(30),
+    [First_Name] NVARCHAR(30),
+    [Surname] NVARCHAR(30),
+    [Email] NVARCHAR(100),
+    [Mobile] NVARCHAR(12),
+	PRIMARY KEY (STUDENT_ID),
+);
